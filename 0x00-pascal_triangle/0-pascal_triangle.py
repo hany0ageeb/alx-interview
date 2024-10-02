e7e6a678aa7a7bfcc80d5410fa6706339f92ba9e
hany0ageeb <hany0ageeb@gmail.com>
2024-10-02 08:22:03 +0200

    adding first line

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-pla pla pla
+module documentation
 """
 def pascal_triangle(n):
     """
-    Pla pla pla
+    function to return pascal's triangle
     """
     if (n <= 0):
         return []
