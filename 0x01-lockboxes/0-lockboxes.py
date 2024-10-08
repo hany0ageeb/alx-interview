f8b935edee7ebc8d5fe0747f6f5d9a2f420c98d8
hany0ageeb <hany0ageeb@gmail.com>
2024-10-08 13:21:19 +0200

    enhancing canUnlockAll

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -19,6 +19,11 @@ def canUnlockAll(boxes):
             if (key not in visited):
                 visited.add(key)
                 visit(boxes[key])
+    n = len(boxes)
+    if (n == 0 or n == 1):
+        return True
+    if (len(boxes[0]) == 0):
+        return False
     visited = set([0])
     visit(boxes[0])
-    return visited == set(range(0, len(boxes)))
+    return visited == set(range(0, n))
