582cde69969f837e6b4c1cc5298265faaf4f0386
hany0ageeb <hany0ageeb@gmail.com>
2024-11-21 17:54:10 +0200

    change algorithm!

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,32 +8,11 @@ You can assume the matrix will have 2 dimensions and will not be empty.
 
 
 def rotate_2d_matrix(matrix):
-    """rotate_2d_matrix
-    """
-    for deg in range(90):
-        top = 0
-        bottom = len(matrix) - 1
-        left = 0
-        right = len(matrix[0]) - 1
-        while left < right and top < bottom:
-            prev = matrix[top + 1][left]
-            for i in range(left, right + 1):
-                curr = matrix[top][i]
-                matrix[top][i] = prev
-                prev = curr
-            top += 1
-            for i in range(top, bottom + 1):
-                curr = matrix[i][right]
-                matrix[i][right] = prev
-                prev = curr
-            right -= 1
-            for i in range(right, left-1, -1):
-                curr = matrix[bottom][i]
-                matrix[bottom][i] = prev
-                prev = curr
-            bottom -= 1
-            for i in range(bottom, top-1, -1):
-                curr = matrix[i][left]
-                matrix[i][left] = prev
-                prev = curr
-            left += 1
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
